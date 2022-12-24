25d8119e8df7272f4a27db09d1b87a860fd93f5f
wsd <wsd@qq.com>
2022-12-24 22:57:12 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @Author: wsd
+ * @Create: 2022-12-24 22:52
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
+
+
